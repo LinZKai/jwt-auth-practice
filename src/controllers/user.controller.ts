517a64b5569e5188edf25fc469555e27c635fd0a
LinZKai <123055677+LinZKai@users.noreply.github.com>
2026-02-24 11:14:32 +0800

    Clean up comments in user controller
    Removed commented code regarding future implementation.

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,8 +2,6 @@ import { Response } from "express";
 import { AuthedRequest } from "../middlewares/auth";
 
 export async function me(req: AuthedRequest, res: Response) {
-  // 這裡先直接回 JWT 裡的資訊
-  // 下一步我們可以改成：用 req.user.sub 去 DB 查 user（更完整）
   return res.json({
     user: req.user,
   });
